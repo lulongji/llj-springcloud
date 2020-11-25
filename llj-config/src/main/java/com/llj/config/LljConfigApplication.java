9ba62b2dfb2fda0665a198e436e43f2bfa5739a5
目瞪口呆的沙拉 <lulongji2011@163.com>
2020-11-25 11:18:42 +0800

    初始化

---
+++ llj-config/src/main/java/com/llj/config/LljConfigApplication.java
@@ -0,0 +1,13 @@
+package com.llj.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LljConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LljConfigApplication.class, args);
+    }
+
+}
---
+++ llj-config/src/test/java/com/llj/config/LljConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llj.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LljConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
